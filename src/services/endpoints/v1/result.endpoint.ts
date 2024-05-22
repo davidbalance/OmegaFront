c6f68c81de6b1f773074974d67b29093719b51cd
Damoz <panchitodmun@gmail.com>
2024-05-22 15:31:42 -0500

    Change result find endpoint

--- src/services/endpoints/v1/result.endpoint.ts
+++ src/services/endpoints/v1/result.endpoint.ts
@@ -2,7 +2,8 @@ import { ResultAPI } from "..";
 import { root } from "../config";
 
 export const ResultEndpoint: ResultAPI = {
-    FIND: `${root}/results/doctor`,
+    FIND: `${root}/results`,
+    // FIND: `${root}/results/doctor`,
     FIND_FILE: (key: string) => `${root}/results/file/${key}`,
     FIND_ONE_AND_UPDATE_REPORT: (key: string) => `${root}/results/report/${key}`,
     FIND_ONE_AND_UPDATE_DISEASE: (key: string) => `${root}/results/${key}`,
