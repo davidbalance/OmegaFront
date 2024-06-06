0642a044be3ed397fa07660e1c883f860378f2a7
Damoz <panchitodmun@gmail.com>
2024-06-06 08:08:22 -0500

    :sparkles: Fix omega page

--- src/app/omega/page.tsx
+++ src/app/omega/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react';
-import { Container, Text, useMantineTheme } from '@mantine/core';
+import { Text, useMantineTheme } from '@mantine/core';
 import { ModularBox } from '@/components/modular-box/ModularBox';
 import classes from './page.module.css';
 
@@ -9,26 +9,24 @@ const Landing: React.FC = () => {
   const theme = useMantineTheme();
 
   return (
-    <ModularBox>
-      <Container size={700} className={classes.inner}>
-        <h1 className={classes.title}>
-          <Text
-            fw='bolder'
-            component="span"
-            variant="gradient"
-            gradient={{ from: theme.colors["omegaColors"][3], to: theme.colors["omegaColors"][6] }}
-            inherit>
-            OMEGA
-          </Text>{' '}
-          Sistema de reporteria medica
-        </h1>
+    <ModularBox h='100%' align='center' justify='center'>
+      <h1 className={classes.title}>
+        <Text
+          fw='bolder'
+          component="span"
+          variant="gradient"
+          gradient={{ from: theme.colors["omegaColors"][3], to: theme.colors["omegaColors"][6] }}
+          inherit>
+          OMEGA
+        </Text>{' '}
+        Sistema de reporteria medica
+      </h1>
 
-        <Text className={classes.description} c="dimmed" size='sm' mt='md'>
-          Sistema enfocado en la gestion de reportes
-          <br />- Acceso a los archivos almacenados en nuestros servidores.
-          <br />- Elaboracion de reportes.
-        </Text>
-      </Container>
+      <Text className={classes.description} c="dimmed" size='sm' mt='md'>
+        Sistema enfocado en la gestion de reportes
+        <br />- Acceso a los archivos almacenados en nuestros servidores.
+        <br />- Elaboracion de reportes.
+      </Text>
     </ModularBox>
   )
 }
