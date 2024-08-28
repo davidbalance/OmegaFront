d8850b971f0d0f9d9c538ce93eecf325922daaf1
Damoz <panchitodmun@gmail.com>
2024-08-28 12:20:08 -0500

    Responsive app sell

--- src/app/omega/_components/omega-shell/omega-shell-navbar.tsx
+++ src/app/omega/_components/omega-shell/omega-shell-navbar.tsx
@@ -2,23 +2,40 @@
 
 import React from 'react'
 import clsx from 'clsx'
-import { Box } from '@mantine/core'
+import { Box, Drawer, useMantineTheme } from '@mantine/core'
 
 import classes from './omega-shell.module.css'
 import { useOmegaShell } from './omega-shell.context'
+import { useMediaQuery } from '@mantine/hooks'
 
 interface OmegaShellNavbarProps {
     children: React.ReactNode
 }
 const OmegaShellNavbar: React.FC<OmegaShellNavbarProps> = ({ children }) => {
 
-    const { opened } = useOmegaShell();
+    const { breakpoints } = useMantineTheme();
+    const { opened, toggle } = useOmegaShell();
+    const match = useMediaQuery(`(min-width: ${breakpoints.md})`);
 
     return (
-        <Box component='nav'
-            className={clsx(classes.shellNavbarRoot, classes.shellBodyContainer, classes.shellModule, { [classes.closed]: !opened })}>
-            {children}
-        </Box>
+        !match
+            ? <Drawer
+                component='nav'
+                opened={opened}
+                onClose={toggle}
+                className={clsx(classes.shellNavbarRoot, classes.closed)}
+                overlayProps={{ backgroundOpacity: 0.5, blur: 4 }}
+                size='100%'
+                closeButtonProps={{
+                    size: 'sm'
+                }}>
+                {children}
+            </Drawer>
+            : <Box component='nav'
+                className={clsx(classes.shellNavbarRoot, classes.shellBodyContainer, classes.shellModule, { [classes.closed]: !opened })}>
+                {children}
+            </Box>
+
     )
 }
 
