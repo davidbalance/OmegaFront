a6f35d1df9d605ba0ddee753ce109871da522ba9
Damoz <panchitodmun@gmail.com>
2024-11-06 08:16:25 -0500

    Remove tree download

--- src/app/omega/misc/page.tsx
+++ src/app/omega/misc/page.tsx
@@ -5,9 +5,7 @@ import DiseaseReportForm from './_components/disease-report-form';
 import LocationInputSelect from './_components/location-input-select';
 import YearInputSelect from './_components/year-input-select';
 import { ModularBox } from '@/components/modular/box/ModularBox';
-import { Box, rem, ScrollArea, Select, TextInput, Title } from '@mantine/core';
-import FileTreeDownloaderForm from '@/app/omega/misc/_components/file-tree-downloader-form';
-import { IconId } from '@tabler/icons-react';
+import { Box, ScrollArea, Title } from '@mantine/core';
 import ModularLayout from '@/components/modular/layout/ModularLayout';
 import { retriveMedicalOrderProcesses } from '@/server/medical-order.actions';
 
@@ -34,41 +32,6 @@ const OmegaReportDiseasePage: React.FC = async () => {
               options={corporativeGroupOptions}
               showCompany />
           </DiseaseReportForm>
-          {/* <Box style={{ marginTop: rem(2) }} />
-          <ModularBox>
-            <Box style={{ flexShrink: 0 }}>
-              <Title order={4} component='span'>Descarga arbol de archivos</Title>
-            </Box>
-          </ModularBox>
-          <FileTreeDownloaderForm>
-            <YearInputSelect
-              options={yearOptions} />
-            <LocationInputSelect
-              options={corporativeGroupOptions}
-              showCompany
-              showBranch />
-            <Select
-              data={processes}
-              checkIconPosition="left"
-              searchable
-              clearable
-              defaultDropdownOpened={false}
-              maxDropdownHeight={200}
-              name='process'
-              label="Proceso"
-              placeholder="Escoge un proceso"
-              nothingFoundMessage="Proceso no encontrado..." />
-            <TextInput
-              w='100%'
-              name='dni'
-              size='sm'
-              type='text'
-              label='Paciente'
-              placeholder='175******'
-              leftSection={
-                <IconId style={{ width: rem(16), height: rem(16) }} />
-              } />
-          </FileTreeDownloaderForm> */}
         </ModularLayout>
       </ScrollArea>
     </Box>
