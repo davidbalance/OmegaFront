d139ba47bfb899a02cc1ecccebb12968b3586982
Damoz <panchitodmun@gmail.com>
2025-03-19 08:10:24 -0500

    Remove testData from Record forms

--- src/app/omega/record/create/certificate/page.tsx
+++ src/app/omega/record/create/certificate/page.tsx
@@ -10,28 +10,6 @@ import MedicalFitnessForJobForm from '@/components/record/medical-fitness-for-jo
 import RecommendationForm from '@/components/record/recommendation-form';
 import CertificateRetirementEvaluationForm from './_components/certificate-retirement-evaluation';
 import PreviewCertificateRecord from './_components/preview-certificate-record';
-import { CertificateRecordPayload } from '@/server/record/create-record/certificate-record';
-
-const testData: CertificateRecordPayload = {
-    "patientFirstName": "WILLAN",
-    "patientMiddleName": "HERNAN",
-    "patientLastName": "FERNANDEZ",
-    "patientSecondLastName": "FLORES",
-    "patientGender": "male",
-    "companyName": "COLEGIO MENOR",
-    "companyRUC": "1791754794001",
-    "companyCIU": "CIU",
-    "institutionHealthFacility": "Omega Salud Ocupacional",
-    "jobPosition": "SAMPLE",
-    "generalData": "reintegrate",
-    "medicalFitnessType": "fit-observation",
-    "medicalFitnessLimitation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec purus ut lacus mollis vestibulum. Morbi sagittis risus sem, nec vestibulum lectus vestibulum in. Praesent iaculis quam nisi, ut ultrices leo volutpat vel. Cras aliquam elit nec neque egestas, a varius lacus cursus. Vestibulum fermentum suscipit lacus eget cursus. Etiam eu molestie mi. Sed non aliquam neque. Aenean pellentesque lorem eleifend egestas tincidunt. Vestibulum eleifend leo id augue commodo gravida. Donec ultrices eros ut ligula dignissim, in ultricies erat pellentesque. Ut accumsan purus a egestas molestie. Nam fringilla a turpis sit amet eleifend.",
-    "medicalFitnessObservation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec purus ut lacus mollis vestibulum. Morbi sagittis risus sem, nec vestibulum lectus vestibulum in. Praesent iaculis quam nisi, ut ultrices leo volutpat vel. Cras aliquam elit nec neque egestas, a varius lacus cursus. Vestibulum fermentum suscipit lacus eget cursus. Etiam eu molestie mi. Sed non aliquam neque. Aenean pellentesque lorem eleifend egestas tincidunt. Vestibulum eleifend leo id augue commodo gravida. Donec ultrices eros ut ligula dignissim, in ultricies erat pellentesque. Ut accumsan purus a egestas molestie. Nam fringilla a turpis sit amet eleifend.",
-    "retirementEvaluationDone": true,
-    "retirementEvaluationCondition": "presuntive",
-    "retirementEvaluationConditionWithJob": "yes",
-    "recommendationDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec purus ut lacus mollis vestibulum. Morbi sagittis risus sem, nec vestibulum lectus vestibulum in. Praesent iaculis quam nisi, ut ultrices leo volutpat vel. Cras aliquam elit nec neque egestas, a varius lacus cursus. Vestibulum fermentum suscipit lacus eget cursus. Etiam eu molestie mi. Sed non aliquam neque. Aenean pellentesque lorem eleifend egestas tincidunt. Vestibulum eleifend leo id augue commodo gravida. Donec ultrices eros ut ligula dignissim, in ultricies erat pellentesque. Ut accumsan purus a egestas molestie. Nam fringilla a turpis sit amet eleifend."
-}
 
 interface RecordCertificatePageProps {
     searchParams: { [key: string]: string | string[] | undefined }
@@ -73,7 +51,6 @@ const RecordCertificatePage: React.FC<RecordCertificatePageProps> = async ({
                 ]}
                 patientDni={patientDni}
                 initialData={{
-                    ...testData,
                     patientFirstName: patientFirstName,
                     patientMiddleName: patientMiddleName,
                     patientLastName: patientLastName,
--- src/app/omega/record/create/retirement/page.tsx
+++ src/app/omega/record/create/retirement/page.tsx
@@ -4,7 +4,6 @@ import { CorporativeOption } from '@/server/corporative/server_types';
 import { retriveClientByDni } from '@/server/medical_client/actions';
 import React from 'react'
 import StepperRetirementRecordForm from './_components/stepper-retirement-record-form';
-import PlaceholderForm from './_components/place-holder-form';
 import RetirementInstitutionForm from './_components/retirement-institution-form';
 import RetirementActivityAndRiskForm from './_components/retirement-activity-and-risk-form';
 import MedicalAndSurgicalHistoryForm from '@/components/record/medical-and-surgical-history-form';
@@ -17,132 +16,6 @@ import MedicalDiagnosticForm from '@/components/record/medical-diagnostic-form';
 import RetirementEvaluationForm from './_components/retirement-evaluation-form';
 import RecommendationForm from '@/components/record/recommendation-form';
 import PreviewRetirementRecord from './_components/preview-retirement-record';
-import { RetirementRecordPayload } from '@/server/record/create-record/retirement-record';
-
-const testData: RetirementRecordPayload = {
-    "patientFirstName": "WILLAN",
-    "patientMiddleName": "HERNAN",
-    "patientLastName": "FERNANDEZ",
-    "patientSecondLastName": "FLORES",
-    "patientGender": "male",
-    "companyName": "COLEGIO MENOR",
-    "companyRUC": "1791754794001",
-    "companyCIU": "CIU",
-    "institutionHealthFacility": "Omega Salud Ocupacional",
-    "workStartDate": new Date("2025-03-15T00:13:58.166Z"),
-    "workingTime": 0,
-    "workingEndDate": new Date("2025-03-15T00:13:58.166Z"),
-    "jobPosition": "GERENTE",
-    "institutionActivities": [
-        {
-            "activity": "SAMPLE",
-            "risk": "SAMPLE"
-        },
-        {
-            "activity": "SAMPLE",
-            "risk": "SAMPLE"
-        },
-        {
-            "activity": "SAMPLE",
-            "risk": "SAMPLE"
-        }
-    ],
-    "medicalAndSurgicalHistory": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec purus ut lacus mollis vestibulum. Morbi sagittis risus sem, nec vestibulum lectus vestibulum in. Praesent iaculis quam nisi, ut ultrices leo volutpat vel. Cras aliquam elit nec neque egestas, a varius lacus cursus. Vestibulum fermentum suscipit lacus eget cursus. Etiam eu molestie mi. Sed non aliquam neque. Aenean pellentesque lorem eleifend egestas tincidunt. Vestibulum eleifend leo id augue commodo gravida. Donec ultrices eros ut ligula dignissim, in ultricies erat pellentesque. Ut accumsan purus a egestas molestie. Nam fringilla a turpis sit amet eleifend.",
-    "jobAccidentHappened": true,
-    "jobAccidentDate": new Date("2025-03-15T00:16:50.539Z"),
-    "jobAccidentDescription": "SAMPLE",
-    "jobAccidentObservation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec purus ut lacus mollis vestibulum. Morbi sagittis risus sem, nec vestibulum lectus vestibulum in. Praesent iaculis quam nisi, ut ultrices leo volutpat vel. Cras aliquam elit nec neque egestas, a varius lacus cursus. Vestibulum fermentum suscipit lacus eget cursus. Etiam eu molestie mi. Sed non aliquam neque. Aenean pellentesque lorem eleifend egestas tincidunt. Vestibulum eleifend leo id augue commodo gravida. Donec ultrices eros ut ligula dignissim, in ultricies erat pellentesque. Ut accumsan purus a egestas molestie. Nam fringilla a turpis sit amet eleifend.",
-    "occupationalDiseaseHappened": true,
-    "occupationalDiseaseDate": new Date("2025-03-15T00:16:57.550Z"),
-    "occupationalDiseaseDescription": "SAMPLE",
-    "occupationalDiseaseObservation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec purus ut lacus mollis vestibulum. Morbi sagittis risus sem, nec vestibulum lectus vestibulum in. Praesent iaculis quam nisi, ut ultrices leo volutpat vel. Cras aliquam elit nec neque egestas, a varius lacus cursus. Vestibulum fermentum suscipit lacus eget cursus. Etiam eu molestie mi. Sed non aliquam neque. Aenean pellentesque lorem eleifend egestas tincidunt. Vestibulum eleifend leo id augue commodo gravida. Donec ultrices eros ut ligula dignissim, in ultricies erat pellentesque. Ut accumsan purus a egestas molestie. Nam fringilla a turpis sit amet eleifend.",
-    "vitalSignsBloodPressure": 0,
-    "vitalSignsTemperature": 45,
-    "vitalSignsHeartRate": 45,
-    "vitalSignsOxygenSaturation": 45,
-    "vitalSignsRespiratoryRate": 0,
-    "vitalSignsWeight": 45,
-    "vitalSignsSize": 0,
-    "vitalSignsMassIndex": 45,
-    "vitalSignsAbdominalPerimeter": 0,
-    "examSkinScar": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "examSkinTattoo": "",
-    "examSkinLesions": "",
-    "examEyeEyelids": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "examEyeConjunctiva": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "examEyePupils": "",
-    "examEyeCorneas": "",
-    "examEyeMotility": "",
-    "examEarAuditoryExternal": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "examEarAuricle": "",
-    "examEarEardrum": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "examPharynxLips": "",
-    "examPharynxTongue": "",
-    "examPharynxPharynx": "",
-    "examPharynxTonsils": "",
-    "examPharynxTeeth": "",
-    "examNosePartition": "",
-    "examNoseTurbinates": "",
-    "examNoseMucousMembranes": "",
-    "examNoseParanasalSinuses": "",
-    "examNeckThyroid": "",
-    "examNeckMobility": "",
-    "examChestBreast": "",
-    "examChestHeart": "",
-    "examChestLungs": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "examChestRibCage": "",
-    "examAbdomenViscera": "",
-    "examAbdomenAbdominalWall": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "examColumnFlexibility": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    "examColumnDeviation": "",
-    "examColumnPain": "",
-    "examPelvis": "",
-    "examPelvisGenitals": "",
-    "examLimbVascular": "",
-    "examLimbUpper": "",
-    "examLimbLower": "",
-    "examNeurologicForce": "",
-    "examNeurologicSensitivity": "",
-    "examNeurologicGait": "",
-    "examNeurologicReflex": "",
-    "generalExamResults": [
-        {
-            "date": new Date("2025-03-15T00:11:33.422Z"),
-            "exam": "SAMPLE",
-            "result": "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        }
-    ],
-    "generalExamObservation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec purus ut lacus mollis vestibulum. Morbi sagittis risus sem, nec vestibulum lectus vestibulum in. Praesent iaculis quam nisi, ut ultrices leo volutpat vel. Cras aliquam elit nec neque egestas, a varius lacus cursus. Vestibulum fermentum suscipit lacus eget cursus. Etiam eu molestie mi. Sed non aliquam neque. Aenean pellentesque lorem eleifend egestas tincidunt. Vestibulum eleifend leo id augue commodo gravida. Donec ultrices eros ut ligula dignissim, in ultricies erat pellentesque. Ut accumsan purus a egestas molestie. Nam fringilla a turpis sit amet eleifend.",
-    "diagnostics": [
-        {
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec purus ut lacus mollis vestibulum. Morbi sagittis risus sem, nec vestibulum lectus vestibulum in. Praesent iaculis quam nisi, ut ultrices leo volutpat vel. Cras aliquam elit nec neque egestas, a varius lacus cursus. Vestibulum fermentum suscipit lacus eget cursus. Etiam eu molestie mi. Sed non aliquam neque. Aenean pellentesque lorem eleifend egestas tincidunt. Vestibulum eleifend leo id augue commodo gravida. Donec ultrices eros ut ligula dignissim, in ultricies erat pellentesque. Ut accumsan purus a egestas molestie. Nam fringilla a turpis sit amet eleifend.",
-            "cie": "CIE",
-            "pre": true,
-            "def": true
-        },
-        {
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec purus ut lacus mollis vestibulum. Morbi sagittis risus sem, nec vestibulum lectus vestibulum in. Praesent iaculis quam nisi, ut ultrices leo volutpat vel. Cras aliquam elit nec neque egestas, a varius lacus cursus. Vestibulum fermentum suscipit lacus eget cursus. Etiam eu molestie mi. Sed non aliquam neque. Aenean pellentesque lorem eleifend egestas tincidunt. Vestibulum eleifend leo id augue commodo gravida. Donec ultrices eros ut ligula dignissim, in ultricies erat pellentesque. Ut accumsan purus a egestas molestie. Nam fringilla a turpis sit amet eleifend.",
-            "cie": "CIE",
-            "pre": true,
-            "def": false
-        },
-        {
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec purus ut lacus mollis vestibulum. Morbi sagittis risus sem, nec vestibulum lectus vestibulum in. Praesent iaculis quam nisi, ut ultrices leo volutpat vel. Cras aliquam elit nec neque egestas, a varius lacus cursus. Vestibulum fermentum suscipit lacus eget cursus. Etiam eu molestie mi. Sed non aliquam neque. Aenean pellentesque lorem eleifend egestas tincidunt. Vestibulum eleifend leo id augue commodo gravida. Donec ultrices eros ut ligula dignissim, in ultricies erat pellentesque. Ut accumsan purus a egestas molestie. Nam fringilla a turpis sit amet eleifend.",
-            "cie": "CIE",
-            "pre": false,
-            "def": true
-        },
-        {
-            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec purus ut lacus mollis vestibulum. Morbi sagittis risus sem, nec vestibulum lectus vestibulum in. Praesent iaculis quam nisi, ut ultrices leo volutpat vel. Cras aliquam elit nec neque egestas, a varius lacus cursus. Vestibulum fermentum suscipit lacus eget cursus. Etiam eu molestie mi. Sed non aliquam neque. Aenean pellentesque lorem eleifend egestas tincidunt. Vestibulum eleifend leo id augue commodo gravida. Donec ultrices eros ut ligula dignissim, in ultricies erat pellentesque. Ut accumsan purus a egestas molestie. Nam fringilla a turpis sit amet eleifend.",
-            "cie": "CIE",
-            "pre": false,
-            "def": false
-        }
-    ],
-    "retirementDone": true,
-    "retirementObservation": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec purus ut lacus mollis vestibulum. Morbi sagittis risus sem, nec vestibulum lectus vestibulum in. Praesent iaculis quam nisi, ut ultrices leo volutpat vel. Cras aliquam elit nec neque egestas, a varius lacus cursus. Vestibulum fermentum suscipit lacus eget cursus. Etiam eu molestie mi. Sed non aliquam neque. Aenean pellentesque lorem eleifend egestas tincidunt. Vestibulum eleifend leo id augue commodo gravida. Donec ultrices eros ut ligula dignissim, in ultricies erat pellentesque. Ut accumsan purus a egestas molestie. Nam fringilla a turpis sit amet eleifend.",
-    "recommendationDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec purus ut lacus mollis vestibulum. Morbi sagittis risus sem, nec vestibulum lectus vestibulum in. Praesent iaculis quam nisi, ut ultrices leo volutpat vel. Cras aliquam elit nec neque egestas, a varius lacus cursus. Vestibulum fermentum suscipit lacus eget cursus. Etiam eu molestie mi. Sed non aliquam neque. Aenean pellentesque lorem eleifend egestas tincidunt. Vestibulum eleifend leo id augue commodo gravida. Donec ultrices eros ut ligula dignissim, in ultricies erat pellentesque. Ut accumsan purus a egestas molestie. Nam fringilla a turpis sit amet eleifend."
-}
 
 type RecordRetirementPageProps = {
     searchParams: { [key: string]: string | string[] | undefined }
@@ -191,7 +64,6 @@ const RecordRetirementPage: React.FC<RecordRetirementPageProps> = async ({
                 ]}
                 patientDni={patientDni}
                 initialData={{
-                    ...testData,
                     patientFirstName: patientFirstName,
                     patientMiddleName: patientMiddleName,
                     patientLastName: patientLastName,
