a8ed44d2faafa506f148f9ab42b5ca1252abe5b2
Damoz <panchitodmun@gmail.com>
2026-07-03 15:34:58 -0500

    feat(new-record): update create record page

--- src/app/omega/record/create/femo/page.tsx
+++ src/app/omega/record/create/femo/page.tsx
@@ -8,6 +8,7 @@ import { FemoRecordPayload } from '@/server/record/create-record/femo-record';
 import FemoPatientHistoryForm from '../../../../../components/record/femo/femo-personal-history-form';
 import { PATIENT_GENDER_FEMALE } from '@/server/record/create-record/femo/institution.schema';
 import StepperCreateFemoRecordForm from '@/components/record/femo/stepper-create-femo-record-form';
+import FemoVitalSignsAndAnthropometryForm from '@/components/record/femo/femo-vital-signs-form';
 
 interface RecordFemoPageProps {
     searchParams: { [key: string]: string | string[] | undefined }
@@ -19,16 +20,6 @@ const RecordFemoPage: React.FC<RecordFemoPageProps> = async ({
     const patientDni = typeof searchParams.patientDni === 'string' ? searchParams.patientDni : undefined;
 
     if (!patientDni) return <>Paciente no especificado</>
-
-    const corporativeBaseOptions = await retriveCorporativesOptions();
-    const corporativeOptions = corporativeBaseOptions.map<CorporativeOption>((e) => ({
-        ...e,
-        children: e.children.map(x => ({
-            ...x,
-            label: x.label.split('-')[1],
-            value: x.label.split('-')[0],
-        }))
-    }));
 
     const patient = await retriveClientByDni(patientDni);
 
@@ -52,14 +43,12 @@ const RecordFemoPage: React.FC<RecordFemoPageProps> = async ({
             <StepperCreateFemoRecordForm
                 headers={[
                     { title: 'Datos del profesional', icon: 'medicine' },
-                    { title: 'Datos del establecimiento', description: 'Empresa y usuario', icon: 'building' },
-                    { title: 'Motivo de Consulta', icon: "activity" }
+                    { title: 'Constantes Vitales y Antropometría', icon: "briefcase" },
                 ]}
                 initialData={{ patient: initialPatientData }}
                 patientDni={patientDni}>
                 <ProfessionalDataForm />
-                <InitialInstitutionForm options={corporativeOptions} />
-                <FemoPatientHistoryForm isFemale={patient.patientGender === PATIENT_GENDER_FEMALE} />
+                <FemoVitalSignsAndAnthropometryForm />
             </StepperCreateFemoRecordForm>
         </>
     )
